e881f1924a9ea57fa536d4d997ded2cf00b54279
John Kline <jtkline1205@gmail.com>
2023-12-23 01:05:35 -0500

    decks and shoes

--- main.go
+++ main.go
@@ -3,10 +3,16 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"math/rand"
 	"net/http"
+	"strconv"
+	"sync"
+	"time"
 
 	"github.com/gorilla/mux"
 )
+
+// Structs
 
 type Rank struct {
 	Name           string `json:"name"`
@@ -16,7 +22,116 @@ type Rank struct {
 	Order          uint8  `json:"order"`
 }
 
+type Card struct {
+	RankLabel string
+	Suit      string
+}
+
+type Deck struct {
+	Cards []Card
+}
+
+type Shoe struct {
+	Decks []*Deck
+}
+
+// Package Variables
+
 var ranks []Rank
+var SizeToShoeMap = make(map[int]*Shoe)
+var mutex = &sync.Mutex{}
+
+// Functions
+
+func NewDeck() *Deck {
+	var cards []Card
+
+	for _, suit := range []string{"H", "S", "D", "C"} {
+		for _, rank := range []string{"2", "3", "4", "5", "6", "7", "8", "9", "T", "J", "Q", "K", "A"} {
+			cards = append(cards, Card{RankLabel: rank, Suit: suit})
+		}
+	}
+
+	return &Deck{Cards: cards}
+}
+
+func (s *Shoe) CardsLeft() int {
+	totalCards := 0
+	for _, deck := range s.Decks {
+		totalCards += len(deck.Cards)
+	}
+	return totalCards
+}
+
+func (d *Deck) DrawCard() Card {
+	if len(d.Cards) == 0 {
+		panic("deck is empty")
+	}
+
+	rand.Seed(time.Now().UnixNano())
+	index := rand.Intn(len(d.Cards))
+	drawnCard := d.Cards[index]
+	d.Cards = append(d.Cards[:index], d.Cards[index+1:]...)
+	return drawnCard
+}
+
+func (d *Deck) HasCards() bool {
+	return len(d.Cards) > 0
+}
+
+func (card Card) String() string {
+	var result string
+	result += fmt.Sprintf("%s%s", card.RankLabel, card.Suit)
+	return result
+}
+
+func (d *Deck) String() string {
+	var result string
+	for _, card := range d.Cards {
+		result += fmt.Sprintf("%s%s\n", card.RankLabel, card.Suit)
+	}
+	return result
+}
+
+func NewShoe(numDecks int) *Shoe {
+	var decks []*Deck
+	for i := 0; i < numDecks; i++ {
+		decks = append(decks, NewDeck())
+	}
+	return &Shoe{Decks: decks}
+}
+
+func (s *Shoe) DrawCard() Card {
+	if len(s.Decks) == 0 {
+		panic("shoe is empty")
+	}
+
+	// Choose a random deck from the shoe
+	rand.Seed(time.Now().UnixNano())
+	deckIndex := rand.Intn(len(s.Decks))
+
+	// Draw a card from the selected deck
+	return s.Decks[deckIndex].DrawCard()
+}
+
+func (s *Shoe) HasCards() bool {
+	for _, deck := range s.Decks {
+		if deck.HasCards() {
+			return true
+		}
+	}
+	return false
+}
+
+func (s *Shoe) String() string {
+	var result string
+	for i, deck := range s.Decks {
+		result += fmt.Sprintf("Deck %d:\n%s", i+1, deck)
+	}
+	return result
+}
+
+// Handlers
 
 func GetRankBlackjackValueHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
@@ -50,22 +165,6 @@ func GetRankBaccaratValueHandler(w http.ResponseWriter, r *http.Request) {
 	http.NotFound(w, r)
 }
 
-func GetRankLabelHandler(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-
-	params := mux.Vars(r)
-	itemLabel := params["label"]
-
-	for _, item := range ranks {
-		if item.Label == itemLabel {
-			json.NewEncoder(w).Encode(item.Label)
-			return
-		}
-	}
-
-	http.NotFound(w, r)
-}
-
 func GetRankOrderHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 
@@ -82,8 +181,64 @@ func GetRankOrderHandler(w http.ResponseWriter, r *http.Request) {
 	http.NotFound(w, r)
 }
 
+func DrawCardHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
+	vars := mux.Vars(r)
+	shoeSizeStr := vars["shoeSize"]
+
+	shoeSize, err := strconv.Atoi(shoeSizeStr)
+	if err != nil {
+		http.Error(w, "Invalid shoe size", http.StatusBadRequest)
+		return
+	}
+
+	mutex.Lock()
+	defer mutex.Unlock()
+
+	shoe, found := SizeToShoeMap[shoeSize]
+	if !found {
+		http.NotFound(w, r)
+		return
+	}
+
+	drawnCard := shoe.DrawCard()
+	json.NewEncoder(w).Encode(drawnCard.String())
+	return
+
+}
+
+func CardsLeftHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
+	vars := mux.Vars(r)
+	shoeSizeStr := vars["shoeSize"]
+
+	shoeSize, err := strconv.Atoi(shoeSizeStr)
+	if err != nil {
+		http.Error(w, "Invalid shoe size", http.StatusBadRequest)
+		return
+	}
+
+	shoe, found := SizeToShoeMap[shoeSize]
+	if !found {
+		http.NotFound(w, r)
+		return
+	}
+
+	cardsLeft := shoe.CardsLeft()
+	json.NewEncoder(w).Encode(cardsLeft)
+	return
+}
+
 func main() {
-	router := mux.NewRouter()
+
+	SizeToShoeMap[1] = NewShoe(1)
+	SizeToShoeMap[2] = NewShoe(2)
+	SizeToShoeMap[4] = NewShoe(4)
+	SizeToShoeMap[8] = NewShoe(8)
+	SizeToShoeMap[9] = NewShoe(9)
+	SizeToShoeMap[10] = NewShoe(10)
 
 	ranks = append(ranks, Rank{Name: "Ace", BlackjackValue: 11, BaccaratValue: 1, Label: "A", Order: 1})
 	ranks = append(ranks, Rank{Name: "Two", BlackjackValue: 2, BaccaratValue: 2, Label: "2", Order: 2})
@@ -99,10 +254,12 @@ func main() {
 	ranks = append(ranks, Rank{Name: "Queen", BlackjackValue: 10, BaccaratValue: 0, Label: "Q", Order: 12})
 	ranks = append(ranks, Rank{Name: "King", BlackjackValue: 10, BaccaratValue: 0, Label: "K", Order: 13})
 
+	router := mux.NewRouter()
 	router.HandleFunc("/ranks/{label}/blackjackValue", GetRankBlackjackValueHandler).Methods("GET")
 	router.HandleFunc("/ranks/{label}/baccaratValue", GetRankBaccaratValueHandler).Methods("GET")
-	router.HandleFunc("/ranks/{label}/label", GetRankLabelHandler).Methods("GET")
 	router.HandleFunc("/ranks/{label}/order", GetRankOrderHandler).Methods("GET")
+	router.HandleFunc("/shoes/{shoeSize}/draw", DrawCardHandler).Methods("GET")
+	router.HandleFunc("/shoes/{shoeSize}/cardsLeft", CardsLeftHandler).Methods("GET")
 
 	port := 5001
 	fmt.Printf("Server is running on :%d...\n", port)
